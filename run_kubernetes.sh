3caf29e865ea08571a44393c44868614fba6d4fd
Mihir Pradhan <mihirpradhan2004@gmail.com>
2020-08-21 17:37:17 -0500

    Update Kube config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,4 +1,9 @@
-aws eks --region us-east-1 update-kubeconfig --name prod
-arn=$(aws eks describe-cluster --name prod | grep -Po '"arn": *\K"[^"]*"' | sed 's/"//g')
+aws eks --region us-east-1 update-kubeconfig --name website
+arn=$(aws eks describe-cluster --name website | grep -Po '"arn": *\K"[^"]*"' | sed 's/"//g')
 kubectl config use-context $arn
+kubectl get svc
 kubectl apply -f deployment.yaml
+kubectl get deployments
+kubectl apply -f service.yaml
+kubectl get services
+kubectl get pods -o wide
